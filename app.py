d9213c5089b12af38e8536b70b83be84419f183d
Reynaldo Mamani - Gilat Peru <RMamaniH@gilatla.com>
2022-11-21 11:42:55 -0500

    interface de admin/regionales

--- app.py
+++ app.py
@@ -15,12 +15,17 @@ def regionales():
 def nosotros():
     return render_template('sitio/nosotros.html')
 
-@app.route('/admin')
+@app.route('/admin/')
 def admin_index():
     return render_template('admin/index.html')
+
 @app.route('/admin/login')
 def admin_login():
     return render_template('admin/login.html')
+
+@app.route('/admin/regionales')
+def admin_regionales():
+    return render_template("admin/regionales.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
